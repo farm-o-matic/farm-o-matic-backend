7541753ce61126ace5463c644fa2b602ee55c2ac
Rubellus <neomoth4@gmail.com>
2022-03-30 00:55:32 +0700

    view preset API
    monolith; show a list of planter box settings presets a.k.a. plant profiles

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import {Request,Response,Application} from 'express'
+import {Response, Request, Application} from 'express'
 import * as express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app:Application = express()
 const port:Number = 3000
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 app.get('/', (req:Request, res:Response) => {
     res.send('Well done!')
+})
+
+// view list of plant profile (settings preset)
+app.get('/viewpresets', async (req, res) => {
+    const presets = await prisma.planterboxsettings.findMany({
+        where: { 
+            SettingsID: {
+                startsWith: '701' 
+            }
+        }
+    })
+    res.json(presets)
 })
 
 app.listen(port, () => {

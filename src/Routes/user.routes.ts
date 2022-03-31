cfab8dade9a8177cff7c8b9f32e999476ade42a9
chimauto <62636000+chimauto@users.noreply.github.com>
2022-04-01 00:22:32 +0700

    Login/userRegister/boxRegister/viewUserprofile

--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
 import { Request, Response } from 'express'
 import { register } from '../Controllers/users.controller'
+import { getuser } from '../Controllers/users.controller'
+import { login } from '../Controllers/users.controller'
+import { addbox } from '../Controllers/users.controller'
 const router = Router()
 
-router.get('/register', register)
+
+router.post('/register', register)
+
+router.post('/login', login)
+
+router.get('/getuser/:id', getuser)
+
+router.post('/addbox/:id', addbox)
 
 router.get('/me', async (req: Request, res: Response) => {
     return res.json({ "Status": "ok users" })
--- src/middleware/register.middleware.ts
+++ src/middleware/register.middleware.ts
@@ -1,17 +1,17 @@
-import * as bcrypt from "bcrypt"
+//import * as bcrypt from "bcrypt"
 // import * as jwt from "jsonwebtoken"
-import { prisma } from "../helper/prisma.client"
+//import { prisma } from "../helper/prisma.client"
 // import { Request, Response, NextFunction } from 'express'
 
-interface data {
-    name: string,
-    email: string,
-    password: string
-}
-const userRegister = async (data: data) => {
-    const { name, email } = data
-    const salt = bcrypt.genSaltSync(42)
-    data.password = bcrypt.hashSync(data.password, salt);
+//interface data {
+//    name: string,
+//    email: string,
+//    password: string
+//}
+//const userRegister = async (data: data) => {
+//    const { name, email } = data
+//    const salt = bcrypt.genSaltSync(42)
+//    data.password = bcrypt.hashSync(data.password, salt);
     // const result = await prisma.user.create({
     // })
-}
+//}
